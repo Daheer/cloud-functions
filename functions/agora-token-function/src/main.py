93dcc7263a08fff12e489f275e6e91686a0ba4fa
Ibrahim Dahiru Dahiru <34832399+Daheer@users.noreply.github.com>
2025-05-15 13:25:11 +0100

    Update main.py

--- functions/agora-token-function/src/main.py
+++ functions/agora-token-function/src/main.py
@@ -1,39 +1,36 @@
-from appwrite.client import Client
-from appwrite.services.users import Users
-from appwrite.exception import AppwriteException
 import os
+import time
+import json
+from agora_token_builder import RtcTokenBuilder
 
-# This Appwrite function will be executed every time your function is triggered
 def main(context):
-    # You can use the Appwrite SDK to interact with other services
-    # For this example, we're using the Users service
-    client = (
-        Client()
-        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
-        .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-        .set_key(context.req.headers["x-appwrite-key"])
-    )
-    users = Users(client)
+    # Retrieve Agora credentials from environment variables
+    app_id = os.environ.get("AGORA_APP_ID")
+    app_certificate = os.environ.get("AGORA_APP_CERTIFICATE")
 
+    # Validate that credentials are available
+    if not app_id or not app_certificate:
+        return context.res.json({"error": "Agora credentials are not set."}, status_code=500)
+
+    # Parse request body
     try:
-        response = users.list()
-        # Log messages and errors to the Appwrite Console
-        # These logs won't be seen by your end users
-        context.log("Total users: " + str(response["total"]))
-    except AppwriteException as err:
-        context.error("Could not list users: " + repr(err))
+        body = json.loads(context.req.body)
+        channel_name = body.get("channelName")
+        uid = body.get("uid", 0)  # Default to 0 if not provided
+        expire_time = body.get("expireTime", 3600)  # Default to 1 hour
+    except Exception as e:
+        return context.res.json({"error": "Invalid request body."}, status_code=400)
 
-    # The req object contains the request data
-    if context.req.path == "/ping":
-        # Use res object to respond with text(), json(), or binary()
-        # Don't forget to return a response!
-        return context.res.text("Pong")
+    # Validate required parameters
+    if not channel_name:
+        return context.res.json({"error": "channelName is required."}, status_code=400)
 
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
+    # Generate token
+    current_timestamp = int(time.time())
+    privilege_expired_ts = current_timestamp + expire_time
+
+    token = RtcTokenBuilder.buildTokenWithUid(
+        app_id, app_certificate, channel_name, uid, 1, privilege_expired_ts
     )
+
+    return context.res.json({"token": token})
